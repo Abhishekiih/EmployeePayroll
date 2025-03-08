406a1180386d70253db6eaf7bed095af2ad0a7f3
Abhishekiih <abhiibaghel1011@gmail.com>
2025-03-08 08:13:25 +0530

    Removed logs from repository

--- src/main/java/com/bridgelabz/employeepayrollapp/Controller/EmployeeController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/Controller/EmployeeController.java
@@ -2,12 +2,14 @@ package com.bridgelabz.employeepayrollapp.Controller;
 
 import com.bridgelabz.employeepayrollapp.model.Employee;
 import com.bridgelabz.employeepayrollapp.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j  // Enables logging
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
